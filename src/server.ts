8a8b8a672b3d7af23f6b16a94bc77e61220adaf1
Jonas Kello <jonas.kello@gmail.com>
2020-09-08 00:47:56 +0200

    custom package def

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,35 @@
 import * as grpc from "@grpc/grpc-js";
 import { loadProtoToPackageDefinition } from "./load-proto";
 
-const packageDefinition = loadProtoToPackageDefinition();
+// const packageDefinition = loadProtoToPackageDefinition();
+
+const packageDefinition = {
+  "greet.Greeter": {
+    SayHello: {
+      path: "/greet.Greeter/SayHello",
+      requestStream: false,
+      responseStream: false,
+      requestSerialize: (value: any): Buffer => {
+        console.log("requestSerialize");
+        return undefined as any;
+      },
+      requestDeserialize: (value: any): Buffer => {
+        console.log("requestDeserialize");
+        return undefined as any;
+      },
+      responseSerialize: (value: any): Buffer => {
+        console.log("responseSerialize");
+        return undefined as any;
+      },
+      responseDeserialize: (value: any): Buffer => {
+        console.log("responseDeserialize");
+        return undefined as any;
+      },
+    } as grpc.MethodDefinition<any, any>,
+  },
+};
+
+console.log("packageDefinition", packageDefinition);
 
 const grpcObj = grpc.loadPackageDefinition(packageDefinition) as any;
 
