bfe1527d269fa2c01f401cba7d485c7a2043c49d
Jonas Kello <jonas.kello@gmail.com>
2020-09-08 15:13:26 +0200

    monkey patch

--- src/server-with-monkey-patch.ts
+++ src/server-with-monkey-patch.ts
@@ -3,6 +3,7 @@ import * as ProtoLoader from "@grpc/proto-loader";
 import * as grpc from "@grpc/grpc-js";
 import { customPackageDefinition } from "./custom-package-def";
 import * as TsProtoGreeter from "./proto/greeter";
+import { Writer, Reader } from "protobufjs/minimal";
 
 const PROTO_OPTIONS = {
   keepCase: true,
@@ -13,38 +14,74 @@ const PROTO_OPTIONS = {
 };
 
 // Load proto
-const PROTO_PATH = path.resolve(
-  __dirname,
-  path.join(__dirname, "../src/proto/greeter.proto")
-);
+const PROTO_PATH = path.resolve(__dirname, path.join(__dirname, "../src/proto/greeter.proto"));
 const packageDefinition = ProtoLoader.loadSync(PROTO_PATH, PROTO_OPTIONS);
 
 // const customPackageDef = customPackageDefinition;
 
-const TsProtoPackages: { [packageName: string]: any } = {
-  greet: TsProtoGreeter,
+type TsProtoPackages = { [packageName: string]: any };
+
+type TsProtoMessage<TMessage> = {
+  encode: (message: TMessage, writer?: Writer) => Writer;
+  decode: (input: Uint8Array | Reader, length?: number) => TMessage;
+  fromJSON: (object: any) => TMessage;
+  toJSON: (message: TMessage) => unknown;
 };
 
-for (const [topKey, anyDef] of Object.entries(packageDefinition)) {
-  if (isServiceDefinition(anyDef)) {
-    const [packageName, methodName] = topKey.split(".");
-    for (const [methodKey, methodDef] of Object.entries(anyDef)) {
-      console.log("topKey", topKey);
-      console.log("methodDef", methodKey, methodDef);
-      //   methodDef.requestDeserialize = undefined;
-      const requestTypeName = (methodDef.requestType.type as any).name;
-      const responseTypeName = (methodDef.responseType.type as any).name;
-      //   const TsProto = Greeter as { [key: string]: any };
-      const tsProtoPackage = TsProtoPackages[packageName];
-      console.log("ts proto requestType", tsProtoPackage[requestTypeName]);
-      console.log("ts proto responseType", tsProtoPackage[responseTypeName]);
+monkeyPatchSerializers({
+  greet: TsProtoGreeter,
+});
+
+function monkeyPatchSerializers(tsProtoPackages: TsProtoPackages) {
+  for (const [topKey, anyDef] of Object.entries(packageDefinition)) {
+    // Only patch service definitions
+    if (isServiceDefinition(anyDef)) {
+      const [packageName, methodName] = topKey.split(".");
+      for (const [methodKey, methodDef] of Object.entries(anyDef)) {
+        console.log("topKey", topKey);
+        console.log("methodDef", methodKey, methodDef);
+        //   methodDef.requestDeserialize = undefined;
+        // Get the names of the request and response types from the method defintion
+        const requestTypeName = (methodDef.requestType.type as any).name;
+        const responseTypeName = (methodDef.responseType.type as any).name;
+        // Get the request and response types from ts proto
+        const tsProtoPackage = tsProtoPackages[packageName];
+        const tsProtoRequest = tsProtoPackage[requestTypeName] as TsProtoMessage<any>;
+        const tsProtoResponse = tsProtoPackage[responseTypeName] as TsProtoMessage<any>;
+        console.log("ts proto requestType", tsProtoRequest);
+        console.log("ts proto responseType", tsProtoResponse);
+        // Create the serializer functions
+        const tsProtoSerializers = {
+          requestSerialize(value: any): Buffer {
+            console.log("requestSerialize");
+            const message = tsProtoRequest.fromJSON(value);
+            return tsProtoRequest.encode(message).finish() as Buffer;
+          },
+          requestDeserialize: (bytes: Buffer): object => {
+            console.log("requestDeserialize", bytes);
+            return tsProtoRequest.toJSON(tsProtoRequest.decode(bytes)) as object;
+          },
+          responseSerialize: (value: any): Buffer => {
+            console.log("responseSerialize");
+            const message = tsProtoResponse.fromJSON(value);
+            return tsProtoResponse.encode(message).finish() as Buffer;
+          },
+          responseDeserialize: (bytes: Buffer): object => {
+            console.log("responseDeserialize");
+            return tsProtoResponse.toJSON(tsProtoResponse.decode(bytes)) as object;
+          },
+        };
+        // Do the monkey patch
+        methodDef.requestSerialize = tsProtoSerializers.requestSerialize;
+        methodDef.requestDeserialize = tsProtoSerializers.requestDeserialize;
+        methodDef.responseSerialize = tsProtoSerializers.responseSerialize;
+        methodDef.responseDeserialize = tsProtoSerializers.responseDeserialize;
+      }
     }
   }
 }
 
-function isServiceDefinition(
-  anyDef: ProtoLoader.AnyDefinition
-): anyDef is ProtoLoader.ServiceDefinition {
+function isServiceDefinition(anyDef: ProtoLoader.AnyDefinition): anyDef is ProtoLoader.ServiceDefinition {
   // ServiceDefinition | MessageTypeDefinition | EnumTypeDefinition
   if (typeof anyDef === "object") {
     const obj = anyDef as { [key: string]: { path?: string } };
@@ -76,10 +113,6 @@ server.addService(serviceDef, serviceImpl);
 // Create and start server
 const port = 3002;
 console.log(`Listening on ${port}`);
-server.bindAsync(
-  `0.0.0.0:${port}`,
-  grpc.ServerCredentials.createInsecure(),
-  () => {
-    server.start();
-  }
-);
+server.bindAsync(`0.0.0.0:${port}`, grpc.ServerCredentials.createInsecure(), () => {
+  server.start();
+});
